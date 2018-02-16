7818fe471cb4f2e23ec4841440a4908cf6569c90
Jonatan Hamberg <jonatan.hamberg@cs.helsinki.fi>
2018-02-16 13:04:20 +0200

    Add a script for generating, building and publishing protocol

---
+++ gen-publish.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+if [ ! -d src/main/java ]; then mkdir -p src/main/java; fi
+thrift -gen java thrift/CaratProtocol.thrift && cp -r gen-java/* src/main/java/. && rm -rf gen-java/
+./gradlew build publishToMavenLocal
